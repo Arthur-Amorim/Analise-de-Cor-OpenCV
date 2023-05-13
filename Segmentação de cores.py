73808c47e6217cce08419878d20bac552e40cab2
Arthur Henrique <124002732+Arthur-Amorim@users.noreply.github.com>
2023-05-13 14:25:02 -0300

    Update Segmentação de cores.py
    Ultima atualização

--- Segmentação de cores.py
+++ Segmentação de cores.py
@@ -1,91 +1,100 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
+#   O código abaixo recebe duas imagens de verduras, descarta o fundo identifica faixas de cores indicadas
+#   Funciona para extensões .jpg, .jpeg, .png, .pdf
 
-img = cv.imread("exemplo1.jpg")
-altura,largura,canais = np.shape(img)
-scale = altura/largura
-altura_n = 700
-largura_n = altura_n / scale
+#   Recebe das imagens
+img1 = cv.imread("exemplo1.jpg")
 
+#   Copia das imagens para futura edicao
+img1_cp = img1.copy()  # copia da imagem 1
 
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+#   Obtem dimensao das imagens
+altura1,largura1,canais1 = np.shape(img1)
 
+#   Transforma imagem do espaço BGR em HSV
+hsv1 = cv.cvtColor(img1, cv.COLOR_BGR2HSV)
+
+#   Define faixas de cores a serem mantidas na imagem
+
+        # Faixa superior e inferior de verde claro
 lowerVc = np.array([35, 60,130])
 upperVc = np.array([70, 255, 255])
-#
+
+        # Faixa superior e inferior de verde escuro
 lowerVe = np.array([35, 60,35])
 upperVe = np.array([70, 255, 130])
-#
+
+        # Faixa superior e inferior de amarelo
 lowerAmc = np.array([25,80, 100])
 upperAmc = np.array([35, 255, 255])
-#
+
+        # Faixa superior e inferior de marrom
 lowerMar = np.array([25, 80, 30])
 upperMar = np.array([35, 255, 100])
-#
-maskVc = cv.inRange(hsv, lowerVc, upperVc)
-maskAmc = cv.inRange(hsv, lowerAmc, upperAmc)
-maskVe = cv.inRange(hsv, lowerVe, upperVe)
-maskMar = cv.inRange(hsv, lowerMar, upperMar)
-maskUtil = maskVe + maskVc + maskAmc + maskMar
-#colorida = cv.cvtColor(maskUtil, cv.COLOR_BGR2HSV)
+
+#   Cria mascaras de cada faixa de cor para imagem 1 e 2
+maskVc = cv.inRange(hsv1, lowerVc, upperVc)          # Faixa de verde claro
+maskAmc = cv.inRange(hsv1, lowerAmc, upperAmc)       # Faixa de amarelo
+maskVe = cv.inRange(hsv1, lowerVe, upperVe)          # Faixa de verde escuro
+maskMar = cv.inRange(hsv1, lowerMar, upperMar)       # Faixa de marrom
+
+#   Junta todas as faixas em uma máscara total
+maskUtil1 = maskVe + maskVc + maskAmc + maskMar
 
 countVc = 0
 countAmc = 0
 countVe = 0
 countMar = 0
-
-for i in range(0,altura):
-    for j in range(0,largura):
+for i in range(0,altura1):
+    for j in range(0,largura1):
+        # O bloco abaixo é responsável por colorir as máscaras criadas acima, com o intuito de visualização das faixas
+        # Mesmo trabalhando com o espaço de cor HSV, as cores indicadas entre chaves são em RGB
+        # as variáveis cont<cor> são usadas para contar quantidade de pixeis de cada faixa para possível cálculo de porcentagem
         if maskVc[i][j] != 0:
-            hsv[i][j] = [57,255,20]
+            img1_cp[i][j] = [57,255,20]
             countVc += 1
         elif maskAmc[i][j] != 0:
-            hsv[i][j] = [255,255,0]
+            img1_cp[i][j] = [255,255,0]
             countAmc += 1
         elif maskMar[i][j] != 0:
-            hsv[i][j] = [150,75,0]
+            img1_cp[i][j] = [150,75,0]
             countMar += 1
         elif maskVe[i][j] != 0:
-            hsv[i][j] = [50, 100, 50]
+            img1_cp[i][j] = [50, 100, 50]
             countVe += 1
+        # Elimina tudo na imagem que não está dentro das faixas detectadas (pinta fundo de preto)
         else:
-            hsv[i][j] = [0,0,0]
+            img1_cp[i][j] = [0,0,0]
 
-hsv_padrao = cv.resize(hsv,(int(largura_n),int(altura_n)))
-
-
+#   Conta os pixeis uteis da imagem (verdura, sem o fundo preto)
 n_pixels_uteis = countVc + countVe + countAmc + countMar
 
-#cv.imshow("Image", hsv_padrao)
-#cv.imshow("MaskVc", maskVc)
-#cv.imshow("MaskAmc", maskAmc)
-#cv.imshow("MaskVe", maskVe)
-#cv.imshow("MaskMar", maskMar)
-#cv.imshow("maskUtil", maskUtil)
-
+#   Calculo da porcentagem de cada faixa
 porcVerde_claro = (countVc/n_pixels_uteis)*100
 porcVerde_escuro = (countVe/n_pixels_uteis)*100
 porcAmarelo_claro = (countAmc/n_pixels_uteis)*100
 porcMarrom = (countMar/n_pixels_uteis)*100
 
+#   Printa na tela a porcentagem de cada faixa
 print(" A figura apresenta {:.2f}% de Verde claro ".format(porcVerde_claro))
 print(" A figura apresenta {:.2f}% de Verde escuro ".format(porcVerde_escuro))
 print(" A figura apresenta {:.2f}% de Amarelo ".format(porcAmarelo_claro))
 print(" A figura apresenta {:.2f}% de Marrom ".format(porcMarrom))
 
+
 f, axs = plt.subplots(1,2)
-axs[0].imshow(img)
+axs[0].imshow(img1)
 axs[0].axis("off")
-axs[1].imshow(hsv_padrao)
+
+axs[1].imshow(img1_cp)
 axs[1].axis("off")
 
-#axs[1].legend(color = ([0,0,0],[1,1,1],[2,2,2],[3,3,3]), label = ("tes","te","to","mate"), bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
-
 plt.show()
-
 cv.waitKey(0)
+
+
 
 
